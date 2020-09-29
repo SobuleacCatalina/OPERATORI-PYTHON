f8a2caa4a9b8dd840fdc900adc5643a76026530b
SobuleacCatalina <71014246+SobuleacCatalina@users.noreply.github.com>
2020-09-29 16:19:42 +0300

    Update problema_2.py

--- problema_2.py
+++ problema_2.py
@@ -1,9 +1,9 @@
-print('problema 2 Sobuleac Catalina')
+print("problema 2 Sobuleac Catalina")
 a=5
 b=2
-print(a,'+',b,'=',a+b)
-print(a,'-',b,'=',a-b)
-print(a,'//',b,'=',a//b)
-print(a,'%',b,'=',a%b)
-print(a,'*',b,'=',a*b)
-print(a,'**',b,'=',a**b)
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"//",b,"=",a//b)
+print(a,"%",b,"=",a%b)
+print(a,"*",b,"=",a*b)
+print(a,"**",b,"=",a**b)
