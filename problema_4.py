c8fef11dd40192f8f5e7e9800df9b5cb5655745d
SobuleacCatalina <71014246+SobuleacCatalina@users.noreply.github.com>
2020-09-29 16:23:06 +0300

    Update problema_4.py

--- problema_4.py
+++ problema_4.py
@@ -1,7 +1,7 @@
-print('problema 4 Sobuleac Catalina')
+print("problema 4 Sobuleac Catalina")
 n=5
-print(n,'metri =',n*100,'centimetri')
-print(n,'kilograme =',n*1000000,'miligrame')
-print(n,'ani =',n*12,'luni')
-print(n,'ani =',n*52,'saptamani')
-print(n,'ani =',n*365,'zile')
+print(n,"metri =",n*100,"centimetri")
+print(n,"kilograme =",n*1000000,"miligrame")
+print(n,"ani =",n*12,"luni")
+print(n,"ani =",n*52,"saptamani")
+print(n,"ani =",n*365,"zile")
