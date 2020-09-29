bb66a3450757b0a0d18d3c366471edc94b748c79
SobuleacCatalina <71014246+SobuleacCatalina@users.noreply.github.com>
2020-09-29 16:20:55 +0300

    Update problema_3.py

--- problema_3.py
+++ problema_3.py
@@ -1,5 +1,5 @@
-print('problema 3 Sobuleac Catalina')
+print("problema 3 Sobuleac Catalina")
 l=3
-print('Perimetrul suprafetei este egal cu ', 4*l)
-print('Aria tuturor suprafetelor este egala cu ', 6*l**2)
-print('Volumul este egal cu ', l**3)
+print("Perimetrul suprafetei este egal cu ", 4*l)
+print("Aria tuturor suprafetelor este egala cu ", 6*l**2)
+print("Volumul este egal cu ", l**3)
